4eaa8acb88876140f982216feb0317c936a9b889
n3jtj <njlaboratory@gmail.com>
2019-01-17 07:12:45 -0500

    updated socket io and express, added asyncWalk and replaced previous walking function

--- web.js
+++ web.js
@@ -22,59 +22,60 @@ init()
 
 function init() {
 	Mix_Controller.initFoldersAndMixes(function(){
-		loadDirectoryIntoExpress()
+		// loadDirectoryIntoExpress()
 	})
 }
 
-eventEmitter.on("updated", function (dd) {
-    console.log("mix update occured");
-	loadDirectoryIntoExpress()
-});
+// eventEmitter.on("updated", function (dd) {
+//     console.log("mix update occured");
+// 	loadDirectoryIntoExpress()
+// });
 
-function loadDirectoryIntoExpress() {
-	Mix_Controller.importAllMixes(function(allMixes) {
-		var keySet = Object.keys(allMixes)
-		for (var i = 0; i < keySet.length; i++) {
-			app.use(express.static(allMixes[keySet[i]].MUSIC_DIRECTORY))
-		}
-	})
-}
+// function loadDirectoryIntoExpress() {
+// 	Mix_Controller.importAllMixes(function(allMixes) {
+// 		console.log("allMixes",allMixes)
+// 		var keySet = Object.keys(allMixes)
+// 		for (var i = 0; i < keySet.length; i++) {
+// 			app.use(express.static(allMixes[keySet[i]].MUSIC_DIRECTORY))
+// 		}
+// 	})
+// }
 
-io.sockets.on('connection', function (socket) {
-	console.log('Someone connected')
-    socket.on('message', function (data) {
-		Mix_Controller.handleSocketIOCommands(data,function(thisMessage){
-			io.sockets.emit('message', { 
-				message: thisMessage
-			})
-		})			
-    })
-})
+// io.sockets.on('connection', function (socket) {
+// 	console.log('Someone connected')
+//     socket.on('message', function (data) {
+// 		Mix_Controller.handleSocketIOCommands(data,function(thisMessage){
+// 			io.sockets.emit('message', { 
+// 				message: thisMessage
+// 			})
+// 		})			
+//     })
+// })
 
-app.get('*',function(req,res,next) { 
-	next()
-})
-app.get('/getSuffleType',function(req,res){
-	Mix_Controller.getSuffleType(function(msg){
-		res.json(msg)
-	})
-})
-app.get('/getVoteCount',function(req,res){
-	Mix_Controller.getVoteCount(function(msg){
-		res.json(msg)
-	})
-})
-app.get('/getCurrentLibrary',function(req,res){
-	Mix_Controller.getCurrentMixFolder(function(msg){
-		res.json(msg)
-	})
-})
-app.get('/',function(req,res){
-	res.set('Content-Type', 'text/html'); // 'text/html' => mime type
-	res.sendfile(APP_ROOT + '/views/index.html')
-}) 
-app.get('/getSrc',function(req,res){
-	Mix_Controller.getStagedUrl(function(msg){
-		res.json(msg)
-	})
-})
+// app.get('*',function(req,res,next) { 
+// 	next()
+// })
+// app.get('/getSuffleType',function(req,res){
+// 	Mix_Controller.getSuffleType(function(msg){
+// 		res.json(msg)
+// 	})
+// })
+// app.get('/getVoteCount',function(req,res){
+// 	Mix_Controller.getVoteCount(function(msg){
+// 		res.json(msg)
+// 	})
+// })
+// app.get('/getCurrentLibrary',function(req,res){
+// 	Mix_Controller.getCurrentMixFolder(function(msg){
+// 		res.json(msg)
+// 	})
+// })
+// app.get('/',function(req,res){
+// 	res.set('Content-Type', 'text/html'); // 'text/html' => mime type
+// 	res.sendfile(APP_ROOT + '/views/index.html')
+// }) 
+// app.get('/getSrc',function(req,res){
+// 	Mix_Controller.getStagedUrl(function(msg){
+// 		res.json(msg)
+// 	})
+// })
